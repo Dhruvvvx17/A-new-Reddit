866f3aed9f3227946fc4d879fb1feb64e2c464d4
Dhruv Vohra <dhruv.v405@gmail.com>
2020-04-10 17:03:49 +0530

    Added new pages, blank

---
+++ src/components/Trending.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/notFound.css'
+
+class Trending extends Component {
+    state = {  }
+    render() { 
+        return (
+            <React.Fragment>
+                <div>
+                <h1 className="center-screen">
+                        Page Under Construction!
+                    </h1>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default Trending;
