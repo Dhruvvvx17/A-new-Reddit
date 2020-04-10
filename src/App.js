f3b0bcf7b216f3c746418ee597650c2a4cb17759
Dhruv Vohra <dhruv.v405@gmail.com>
2020-04-10 17:05:06 +0530

    Added NavLinks and dependencies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import 'typeface-roboto'
+import NavBar from './components/NavBar';
 import MainFeed from './components/MainFeed';
+import Trending from './components/Trending';
+import CreatePost from './components/CreatePost';
+import Profile from './components/Profile';
+import NotFound from './components/NotFound';
 
-//App First react module. Contains the MainFeed module in it which further contains the navbar and posts.
+import {BrowserRouter, Switch,Route} from 'react-router-dom';
 
+//App First react module. Contains the NavBar and MainFeed module in it which further contains the navbar and posts.
 class App extends Component {
 	state = {  }
 	render() { 
 		return (
 			<React.Fragment>
-				<MainFeed/>
+				<BrowserRouter>
+					<NavBar/>
+					<Switch>
+						<Route path="/" exact component={MainFeed}/>
+						<Route path="/Trending/" exact component={Trending}/>
+						<Route path="/CreatePost/" exact component={CreatePost}/>
+						<Route path="/Profile/" exact component={Profile}/>
+						<Route component={NotFound}/>
+					</Switch>			
+				</BrowserRouter>
 			</React.Fragment>
 		);
 	}
