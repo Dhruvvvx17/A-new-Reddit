9bff97e70b2bf793c757ae43385cf4f780c901fc
Dhruv Vohra <dhruv.v405@gmail.com>
2020-04-12 20:07:23 +0530

    Added axios get call in component did mount

--- src/components/MainFeed.jsx
+++ src/components/MainFeed.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import Posts from './Posts'
+import axios from 'axios';
 
 // MainFeed module contains <Posts>. Each post in posts is loaded further from the <Post> module.
 class MainFeed extends Component {
     
     // Fetch entire Feed from API here
+    // Called immediately after a component is mounted. Setting state here will trigger re-rendering.
+    componentDidMount(){
+        axios.get("https://my-json-server.typicode.com/typicode/demo/posts",{crossdomain: true})  //Replace with appropriate API URL
+            .then(response => {
+                console.log(response);
+                // Set this.state.allPosts to the response.
+                // this.state.allPosts = response IN LIST FORMAT like the example of allPosts
+            })
+            .catch(error => {
+                // Error recovery logic
+                console.log(error);
+            })
+    }
+
+
     state = { 
         allPosts: [
 
@@ -16,7 +32,10 @@ class MainFeed extends Component {
             
             // Post 2 
             {id: 2,title:"Forest",description:"A forest is a large area dominated by trees. Hundreds of more precise definitions of forest are used throughout the world, incorporating factors such as tree density, tree height, land use, legal standing and ecological function.",
-             image: "../images/test_image2.jpg", votes: 34}
+             image: "../images/test_image2.jpg", votes: 34},
+            
+            {id: 3,title:"Mountains",description:"A mountain is a large landform that rises above the surrounding land in a limited area, usually in the form of a peak. A mountain is generally steeper than a hill.",
+             image: "../images/test_image3.jpg", votes: 55} 
         ]
     };
 
